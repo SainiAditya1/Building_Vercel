e33e0041e240dd6122509870a5a3d3bbd884fdf0
Aditya Saini <adityasaini10012001@outlook.com>
2024-02-12 21:55:46 +0530

    completed build-server
    Signed-off-by: Aditya Saini <adityasaini10012001@outlook.com>

--- build-server/script.js
+++ build-server/script.js
@@ -1,5 +1,25 @@
 const { exec } = require('child_process')
+require('dotenv').config()
 const path = require('path')
+const fs = require('fs')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const mime = require('mime-types');
+
+
+
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY
+    }
+})
+
+const PROJECT_ID = process.env.PROJECT_ID
+
+
+
+
 
 async function init() {
     console.log('Executing script.js')
@@ -11,7 +31,30 @@ async function init() {
         console.log('Error', data.toString())
     })
 
-    p.on('close', function() {
+    p.on('close', async function() {
         console.log('Build Complete')
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContents = fs.readdirSync(distFolderPath, { recursive: true})
+
+        for (const file of distFolderContents) {
+            const filePath = path.join(distFolderPath, file)
+            if (fs.lstatSync(filePath).isDirectory()) continue;
+
+            console.log('uploading', filePath)
+
+            const command = new PutObjectCommand({
+                Bucket: 'vercel-clone-mine',
+                Key: `__outputs/${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath)
+            })
+
+            await s3Client.send(command)
+
+            console.log('uploaded', filePath)
+        }
+        console.log('Done.');
     })
 }
+
+init()
